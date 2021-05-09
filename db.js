6880424cd58a5ec07e580b9fb55c0cdd4276d4e9
artempenn <agervets197@gmail.com>
2021-05-09 19:38:09 -0400

    trying elis fix

--- db.js
+++ db.js
@@ -7,7 +7,13 @@ const sequelize = new Sequelize(
     process.env.DATABASE_URL || 
     `postgresql://postgres:${encodeURIComponent(process.env.DB_PASS)}@localhost/dublu`,
     {
-        dialect: 'postgres'
+        dialect: 'postgres',
+  dialectOptions: !process.env.DATABASE_URL.includes('localhost') ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION 
+    },
+  } : {}
     }
 )
 
